3f5257ebab07d244ad83dfa1d68247d3c9a35a0e
Aditya Kulkarni <adityakulkarni520@gmail.com>
2026-01-17 20:27:55 +0530

    Exposing Variables to Blueprints: Part 1

--- Source/Slash1/Private/Items/Item.cpp
+++ Source/Slash1/Private/Items/Item.cpp
@@ -5,11 +5,10 @@
 
 
 // Sets default values
-AItem::AItem()
+AItem::AItem() //:Amplitude(0.25f) [1] - can also initialize here
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -17,32 +16,20 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
-	UWorld* World = GetWorld();
-	
-	//SetActorLocation(FVector(0.f, 0.f, 50.f));
-	//FVector Fwd = GetActorForwardVector();
-	//FVector Loc = GetActorLocation();
-	//DRAW_SPHERE(Loc);
-	//DRAW_LINE(Loc, Loc + Fwd * 100.f);
-	//DRAW_POINT(Loc+Fwd*100.f);
-	//DRAW_VECTOR(Loc, Loc + Fwd * 100.f);
-	
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);		//Delta time = seconds per frame
+	Super::Tick(DeltaTime);		//delta time = seconds per frame
 
-	//movt rate in units of cm/s
-	float movtRate = 50.f;
-	float rotRate = 45.f;
+	RunningTime += DeltaTime;  //keep track of total running time
 
-	//movtRate*DeltaTime(cm/s * s/frame = cm/frame); this is done instead of a normal x.f parameter in the below function so that changing fps doesnt change movt spd
-	AddActorWorldOffset(FVector(movtRate*DeltaTime, 0.f, 0.f));		//since lifetime is -1.f, after each frame, it moves by the given parameters
-	AddActorWorldRotation(FRotator(0.f, rotRate * DeltaTime,0.f));
+	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);		//oscillate up and down over time, 0.5f = height multiplier
+	
+	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));		//move the item up and down over time, based on the deltaZ calculated above, 0.f for X and Y means no movement in those axes
+
 	DRAW_SPHERE_1FRAME(GetActorLocation());				//after each frame, it takes the current location, and offsets based on that
-	DRAW_VECTOR_1FRAME(GetActorLocation(), GetActorLocation()+ GetActorForwardVector()*100);
+	DRAW_VECTOR_1FRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100); //draw forward vector for 1 frame
 }
 
