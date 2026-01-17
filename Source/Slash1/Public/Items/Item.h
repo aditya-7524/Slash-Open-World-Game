6a39b9f2b598c4f3dcb0b83617309b9f8a3b5869
Aditya Kulkarni <adityakulkarni520@gmail.com>
2026-01-17 23:16:49 +0530

    Exposing Variables to Blueprints: Part 2

--- Source/Slash1/Public/Items/Item.h
+++ Source/Slash1/Public/Items/Item.h
@@ -21,9 +21,9 @@ protected:
 	virtual void BeginPlay() override;
 private:
 	float RunningTime;
-	UPROPERTY(EditDefaultsOnly) //can only be edited in the editor, not during gameplay
+	UPROPERTY(EditAnywhere) //can only be edited in the editor, not during gameplay
 	float Amplitude = 0.25;	//how high the item moves up and down, can be assigned in constructor also [1]
 
-	UPROPERTY(EditInstanceOnly)  //can only be edited per instance in the editor, not during gameplay
+	UPROPERTY(EditAnywhere)  //can only be edited per instance in the editor, not during gameplay
 	float TimeConstant = 5.f; //how fast the item moves up and down or period=2*PI/TimeConstant
 };
